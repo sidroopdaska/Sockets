764b92abc4a32809a14028367f1be06f4494cd1a
ss1306 <sid3637@gmail.com>
2016-04-15 11:17:49 +0100

    code that handles single connection and terminates after ACK

---
+++ server.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+void error(const char* msg){
+	perror(msg);
+	exit(1);
+}
+
+int main(int argc, char* argv[]){
+	int socketFD, newSocketFD, portNumber;
+	char readBuffer[256];
+	struct sockaddr_in serverAddr, clientAddr;
+	socklen_t clientLength;
+
+	bzero((char*)&serverAddr, sizeof(serverAddr));
+	bzero(readBuffer, sizeof(readBuffer));
+	
+	if(argc < 2){
+		fprintf(stderr, "ERROR, no port provided!\n");
+		exit(1);
+	}	
+	
+	portNumber = atoi(argv[1]);
+	socketFD = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketFD < 1)
+		error("ERROR opening socket");
+	
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = portNumber;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	
+	if(bind(socketFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+		error("ERROR binding socket");
+
+	listen(socketFD, 5);
+	clientLength =  sizeof(clientAddr);
+	newSocketFD = accept(socketFD, (struct sockaddr*)&clientAddr, &clientLength);
+	if (newSocketFD < 0) {
+		error("ERROR making a new connection");
+	}
+
+	if(read(newSocketFD, readBuffer, sizeof(readBuffer)) < 0)
+		error("ERROR reading from socket!");
+
+	fprintf(stdout, "Here is the message: %s\n", readBuffer);
+
+	const char* writeBuffer = "I got your message";
+
+	if(write(newSocketFD, writeBuffer, strlen(writeBuffer)) < 0)
+		error("ERROR writing to the socket");
+
+	close(newSocketFD);
+	close(socketFD);
+
+	return 0;
+}
