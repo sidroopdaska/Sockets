e73ee886da47960407027e119d52bb46dd5b1b22
ss1306 <sid3637@gmail.com>
2016-04-20 15:32:49 +0100

    Added comments to detail the code flow

--- client.cpp
+++ client.cpp
@@ -1,3 +1,6 @@
+/************************************
+	client.cpp
+*************************************/
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
@@ -27,39 +30,42 @@ int main(int argc, char* argv[]){
 		fprintf(stderr, "Error, incomplete arguments!\n");
 		exit(1);
 	}
-	
 	portNumber = atoi(argv[2]);
-	
-	server = gethostbyname(argv[1]);
-	if (server == NULL){
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
-	}
 
+	// Create a socket with the socket() system call
 	socketFD = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketFD < 0)
 		error("Error opening socket");
-
+		
+	// Obtain server details by using the gethostbyname system call()
 	bzero((char*)&serverAddr, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(portNumber);
+
+	server = gethostbyname(argv[1]);
+	if (server == NULL){
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(1);
+	}
+
 	bcopy((char*)server->h_addr, (char*)&serverAddr.sin_addr.s_addr, server->h_length);
 
+	// Connect the socket to the address of the server using the connect() system call
 	if(connect(socketFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
 		error("ERROR connecting!");
 
+	// Send and receive data
 	cout << "Please enter a message: ";
-	cin >> writeBuffer;
-
+	cin.getline(writeBuffer, sizeof(writeBuffer));
 	if (write(socketFD, writeBuffer, strlen(writeBuffer)) < 0)
 		error("ERROR writing to the socket!");
 
 	bzero(readBuffer, sizeof(readBuffer));
 	if(read(socketFD, readBuffer, sizeof(readBuffer)) < 0)
 		error("ERROR reading from socket!");	
-
 	cout << "\nThe server messsage is : " << readBuffer << endl;
 
+	// Close file descriptors
 	close(socketFD);
 
 	return 0;
--- server.cpp
+++ server.cpp
@@ -1,3 +1,7 @@
+/************************************
+	server.cpp
+*************************************/
+
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
@@ -20,44 +24,53 @@ int main(int argc, char* argv[]){
 	struct sockaddr_in serverAddr, clientAddr;
 	socklen_t clientLength;
 
+	// Initializing buffers to zero.
 	bzero((char*)&serverAddr, sizeof(serverAddr));
 	bzero(readBuffer, sizeof(readBuffer));
 	
+	// Obtaining port number from the command line arguments
 	if(argc < 2){
 		fprintf(stderr, "ERROR, no port provided!\n");
 		exit(1);
 	}	
-	
+
 	portNumber = atoi(argv[1]);
+
+	// Setting up a socket for - Domain: Internet, Type: Stream and Protocol: TCP/IP
 	socketFD = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketFD < 1)
 		error("ERROR opening socket");
 	
+	// Setting up Server IP Address and Port Number (as provided by the user)
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(portNumber);
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
 	
+	// Binding the Socket to the Server IP Addr + Port Number details
 	if(bind(socketFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
 		error("ERROR binding socket");
 
+	// Listen for connection requests from Clients
 	listen(socketFD, 5);
+
+	// Accept a connection with the accept() system call. This call typically blocks
+	// until a client connects with the server.
 	clientLength =  sizeof(clientAddr);
 	newSocketFD = accept(socketFD, (struct sockaddr*)&clientAddr, &clientLength);
 	if (newSocketFD < 0) {
 		error("ERROR making a new connection");
 	}
-
+	
+	// Reading and writing data over the socket once a connection has been established.
 	if(read(newSocketFD, readBuffer, sizeof(readBuffer)) < 0)
 		error("ERROR reading from socket!");
-
 	fprintf(stdout, "Here is the message: %s\n", readBuffer);
 
 	const char* writeBuffer = "I got your message";
-	cout << strlen(writeBuffer) << endl;
-
 	if(write(newSocketFD, writeBuffer, strlen(writeBuffer)) < 0)
 		error("ERROR writing to the socket");
-
+	
+	// Close file descriptors
 	close(newSocketFD);
 	close(socketFD);
 
